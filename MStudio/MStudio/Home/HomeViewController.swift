d3f6d75661142b569ffdcf04b9476b2887f79583
Sang Yu Lee <sangyu857@gmail.com>
2026-02-19 03:12:51 +0900

    CHORE: 프로젝트 생성

---
+++ MStudio/MStudio/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  MStudio
+//
+//  Created by 이상유 on 2026-02-19.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
