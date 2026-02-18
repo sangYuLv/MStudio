b4f19da5916b781da32f6becf12d0242238407d6
Sang Yu Lee <sangyu857@gmail.com>
2026-02-19 06:17:28 +0900

    FEAT: HomeViewModel 구현

---
+++ MStudio/MStudio/Home/HomeViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  HomeViewModel.swift
+//  MStudio
+//
+//  Created by 이상유 on 2026-02-19.
+//
+
+import Combine
+
+final class HomeViewModel {
+
+    @Published var urlState: ValidationState = .none
+    private var enteredURL: String = ""
+
+    private var validationTask: Task<Void, Never>?
+
+    func updateURL(_ str: String) {
+        enteredURL = str
+        urlState = .validating
+        validationTask?.cancel()
+        validationTask = Task { [weak self] in
+            guard let self else { return }
+
+            try? await Task.sleep(nanoseconds: 500_000_000)
+            guard !Task.isCancelled else { return }
+
+            let currentURL = self.enteredURL
+            let result = await self.validateURL(str)
+
+            guard currentURL == self.enteredURL else { return }
+            self.urlState = result
+        }
+    }
+
+    private func validateURL(_ str: String) async -> ValidationState {
+        guard !str.isEmpty else { return .none }
+        // TODO: url 검사
+        return .invalid
+    }
+
+}
---
+++ MStudio/MStudio/Home/ValidationState.swift
@@ -0,0 +1,13 @@
+//
+//  ValidationState.swift
+//  MStudio
+//
+//  Created by 이상유 on 2026-02-19.
+//
+
+enum ValidationState {
+    case none
+    case validating
+    case valid
+    case invalid
+}
