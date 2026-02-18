35e29658a926d751fee79cb663a62efe962bcbc6
Sang Yu Lee <sangyu857@gmail.com>
2026-02-19 07:27:32 +0900

    FEAT: IBAction, IBOutlet 연결

--- MStudio/MStudio/Home/HomeViewController.swift
+++ MStudio/MStudio/Home/HomeViewController.swift
@@ -9,8 +9,21 @@ import UIKit
 
 final class HomeViewController: UIViewController {
 
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var invalidNote: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var pasteButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+    @IBAction func tappedPasteButton() {
+        // TODO: 붙여넣기 기능
+    }
+    
+    @IBAction func tappedPlayButton() {
+        // TODO: 영상 재생 화면으로 전환
+    }
     }
 
 }
