ff9926e7470dc06b18b1506fb1d962d419eaeae5
Sang Yu Lee <sangyu857@gmail.com>
2026-02-19 07:28:16 +0900

    FEAT: Home 뷰 모델과 컨트롤러 바인딩

--- MStudio/MStudio/Home/HomeViewController.swift
+++ MStudio/MStudio/Home/HomeViewController.swift
@@ -5,6 +5,7 @@
 //  Created by 이상유 on 2026-02-19.
 //
 
+import Combine
 import UIKit
 
 final class HomeViewController: UIViewController {
@@ -14,8 +15,12 @@ final class HomeViewController: UIViewController {
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var pasteButton: UIButton!
 
+    private var viewModel: HomeViewModel = .init()
+    private var cancellables: Set<AnyCancellable> = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setBinding()
 
     @IBAction func tappedPasteButton() {
         // TODO: 붙여넣기 기능
@@ -23,6 +28,23 @@ final class HomeViewController: UIViewController {
     
     @IBAction func tappedPlayButton() {
         // TODO: 영상 재생 화면으로 전환
+    }
+
+    // MARK: - Set up
+    private func setBinding() {
+        viewModel.$showInvalidNote
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] state in
+                self?.invalidNote.isHidden = !state
+            }
+            .store(in: &cancellables)
+
+        viewModel.$enablePlayButton
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] state in
+                self?.playButton.isEnabled = state
+            }
+            .store(in: &cancellables)
     }
     }
 
